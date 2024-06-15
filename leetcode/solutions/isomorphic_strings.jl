ec98e54707d4154088aa4260c1001b3fb467e9e3
Misael Cureño <curenosm@gmail.com>
2024-06-15 11:29:05 -0600

    Agregando ejercicios documentados

---
+++ leetcode/solutions/isomorphic_strings.jl
@@ -0,0 +1,23 @@
+function is_isomorphic(s, t)
+    if length(s) != length(t) return false end
+
+    freq = Dict{Char, Int64}()
+    for i in eachindex(s)
+        if !haskey(freq, i)
+            if s[i] in values(freq)
+                return false
+            end
+            
+            freq[s[i]] = t[i]
+        elseif freq[s[i]] != t[i]
+            return false
+        end
+    end
+
+    return true
+end
+
+s = "egg"
+t = "add"
+solution = is_isomorphic(s, t)
+println(solution)
---
+++ leetcode/solutions/maximum_nesting_depth_parentheses.jl
@@ -0,0 +1,33 @@
+"""
+    max_depth(s::String) -> Int64
+
+Nos devuelve la máxima anidación de parentesis
+para la cadena especificada.
+
+# Arguments
+- `s::String` - Cadena que posiblemente contiene los caracteres
+'(' o ')'.
+
+# Returns
+- `Int64` - Cuenta de la maxima profundidad para dicha expresión.
+
+# Examples
+```julia
+julia> max_depth("(1)+((2))+(((3)))")
+3
+```
+"""
+function max_depth(s::String)
+    cur, max = 0, typemin(Int64)
+    for c in s
+        cur += if c == '(' 1 elseif c == ')' -1 else 0 end
+        if cur > max
+            max = cur
+        end
+    end
+    return max
+end
+
+input = "(1+(2*3)+((8)/4))+1"
+output = max_depth(input)
+println(output)
