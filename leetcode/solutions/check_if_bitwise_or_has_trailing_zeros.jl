5dbeed29f40a0c0ec53293f38b4629f3d5a0138a
Misael Cureño <curenosm@gmail.com>
2024-06-23 16:36:49 -0600

    Ejercicios

---
+++ leetcode/solutions/check_if_bitwise_or_has_trailing_zeros.jl
@@ -0,0 +1,14 @@
+function has_trailing_zeros(nums)
+    for i in eachindex(nums)
+        for j in i+1:length(nums)
+            if nums[i] & 1 === 0 && nums[j] & 1 === 0
+                return true
+            end
+        end
+    end
+    false
+end
+
+input = [1, 2]
+output = has_trailing_zeros(input)
+println(output)
---
+++ leetcode/solutions/longest_nice_substring.jl
@@ -0,0 +1,36 @@
+function longest_nice_substring(s::String)
+    str = ""
+
+    for i in eachindex(s)
+        for j in 1:length(s)
+            cur_sub_str = s[i:j]
+            if is_nice(cur_sub_str) && length(cur_sub_str) > length(str)
+                str = cur_sub_str
+            end
+        end
+    end
+
+    return str
+end
+
+function is_nice(s::String)
+    freq = Dict{Char,Int}()
+
+    for c in s
+        freq[c] = get(freq, c, 0) + 1
+    end
+
+    for c in s
+        curU, curL = uppercase(c), lowercase(c)
+        if !haskey(freq, curU) || !haskey(freq, curL)
+            return false
+        end
+    end
+
+    return true
+end
+
+
+input = "YazaAay"
+output = longest_nice_substring(input)
+println(output)
