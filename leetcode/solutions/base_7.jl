57db8392833a707e8ca1c0e1d621c5a3d0659724
Misael Cureño <curenosm@gmail.com>
2024-06-28 20:08:34 -0600

    Ejercicios imperativos

---
+++ leetcode/solutions/base_7.jl
@@ -0,0 +1,23 @@
+function base_7(n::Int)::String
+    if n == 0
+        return "0"
+    end
+    sign = if n >= 0
+        ""
+    else
+        "-"
+    end
+    n, sign = abs(n)
+
+    str = ""
+    while n != 0
+        str *= string(n % 7)
+        n /= 7
+    end
+
+    return sign * reverse(str)
+end
+
+input = 12345
+output = base_7(input)
+println(output)
